9858151b051b9f4aa47a517b889f7e2aab6c5e07
Igor Bieńkowski <igorbienkowski@gmail.com>
2026-02-03 15:52:06 +0100

    test: implement unit tests for BR1 and BR2 rules

--- tests/FunBooksAndVideos.Tests.Unit/Processing/OrderProcessorTests.cs
+++ tests/FunBooksAndVideos.Tests.Unit/Processing/OrderProcessorTests.cs
@@ -1,4 +1,5 @@
-﻿using FunBooksAndVideos.Domain.Entities;
+﻿using FunBooksAndVideos.Application.Processing;
+using FunBooksAndVideos.Domain.Entities;
 using FunBooksAndVideos.Domain.Interfaces;
 using FunBooksAndVideos.Domain.Models;
 using NSubstitute;
@@ -9,19 +10,41 @@ public class OrderProcessorTests
 {
     private readonly IMembershipService _membershipService = Substitute.For<IMembershipService>();
     private readonly IShippingService _shippingService = Substitute.For<IShippingService>();
-    
+    private readonly OrderProcessor _sut; // System Under Test
+
+    public OrderProcessorTests()
+    {
+        _sut = new OrderProcessor(_membershipService, _shippingService);
+    }
+
     [Fact]
     public void Process_Should_ActivateMembership_When_OrderContainsMembership()
     {
         // Arrange
-        var order = new PurchaseOrder(3344656, 4567890, [
+        var customerId = 4567890L;
+        var order = new PurchaseOrder(3344656, customerId, [
             new Membership("Book Club", 20.00m, MembershipType.BookClub)
         ]);
 
         // Act
+        _sut.Process(order);
 
         // Assert
-        
-        Assert.Fail("Fails until OrderProcessor is implemented in Application layer.");
+        _membershipService.Received(1).Activate(customerId, MembershipType.BookClub);
+    }
+
+    [Fact]
+    public void Process_Should_GenerateShippingSlip_When_OrderContainsPhysicalBook()
+    {
+        // Arrange
+        var order = new PurchaseOrder(3344656, 4567890, [
+            new Book("The Girl on the train", 28.50m, IsPhysical: true)
+        ]);
+
+        // Act
+        _sut.Process(order);
+
+        // Assert
+        _shippingService.Received(1).GenerateShippingSlip(order);
     }
 }
