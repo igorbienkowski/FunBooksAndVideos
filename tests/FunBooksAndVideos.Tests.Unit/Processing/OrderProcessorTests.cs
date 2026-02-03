b3a12c43c5152ab290fa7445bb352cdfc764f9f2
Igor Bieńkowski <igorbienkowski@gmail.com>
2026-02-03 15:36:18 +0100

    test: add unit test for membership activation (expecting failure)

---
+++ tests/FunBooksAndVideos.Tests.Unit/Processing/OrderProcessorTests.cs
@@ -0,0 +1,27 @@
+﻿using FunBooksAndVideos.Domain.Entities;
+using FunBooksAndVideos.Domain.Interfaces;
+using FunBooksAndVideos.Domain.Models;
+using NSubstitute;
+
+namespace FunBooksAndVideos.Tests.Unit.Processing;
+
+public class OrderProcessorTests
+{
+    private readonly IMembershipService _membershipService = Substitute.For<IMembershipService>();
+    private readonly IShippingService _shippingService = Substitute.For<IShippingService>();
+    
+    [Fact]
+    public void Process_Should_ActivateMembership_When_OrderContainsMembership()
+    {
+        // Arrange
+        var order = new PurchaseOrder(3344656, 4567890, [
+            new Membership("Book Club", 20.00m, MembershipType.BookClub)
+        ]);
+
+        // Act
+
+        // Assert
+        
+        Assert.Fail("Fails until OrderProcessor is implemented in Application layer.");
+    }
+}
