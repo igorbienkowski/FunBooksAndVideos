3cd622b44fe7ea1cf2cfbb6ff03d9775fab372a5
Igor Bieńkowski <igorbienkowski@gmail.com>
2026-02-03 15:30:26 +0100

    feat(domain): add IMembershipService and IShippingService interfaces

---
+++ src/FunBooksAndVideos.Domain/Interfaces/IMembershipService.cs
@@ -0,0 +1,8 @@
+﻿using FunBooksAndVideos.Domain.Models;
+
+namespace FunBooksAndVideos.Domain.Interfaces;
+
+public interface IMembershipService
+{
+    void Activate(long customerId, MembershipType type);
+}
---
+++ src/FunBooksAndVideos.Domain/Interfaces/IShippingService.cs
@@ -0,0 +1,8 @@
+﻿using FunBooksAndVideos.Domain.Entities;
+
+namespace FunBooksAndVideos.Domain.Interfaces;
+
+public interface IShippingService
+{
+    void GenerateShippingSlip(PurchaseOrder order);
+}
